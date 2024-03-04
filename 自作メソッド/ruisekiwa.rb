512bb3dab8a7832821a00bfe4b7ddc1b17b5c929
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-03-04 12:24:43 +0900

    累積和メソッド作成

---
+++ 自作メソッド/ruisekiwa.rb
@@ -0,0 +1,13 @@
+# 累積和
+# 例　nums = [1 2 3 4 5 6 7 8 9 10]から2番目(3)から7番目(8)までの和を求めたいときに使う
+def calc_sum(nums, n, m)
+    sums = [0]
+    sum = 0
+    (nums.size).times do |i|
+        sum += nums[i]
+        sums << sum
+    end
+    
+    ans = sums[m+1] - sums[n]
+    print ans
+end
