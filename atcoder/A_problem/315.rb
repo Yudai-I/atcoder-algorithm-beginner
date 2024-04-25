11815ed4bb0e3d7d295a973d1b5a73fb815ab775
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-04-25 17:30:23 +0900

    add another solution

--- atcoder/A_problem/315.rb
+++ atcoder/A_problem/315.rb
@@ -3,3 +3,14 @@ excepts = ["a","e","i","o","u"]
 s = s.map {|x| excepts.include?(x) ? "" : x}
 ans = s.compact.join
 print ans
+
+
+# 別解
+# s = gets.chomp.chars
+# s.delete("a")
+# s.delete("i")
+# s.delete("u")
+# s.delete("e")
+# s.delete("o")
+
+# print s.join
